27efea87215dbbaae177793e76850a7ccad3a454
Cameron Dawson <cdawson@mozilla.com>
2011-09-26 16:42:25 -0700

    fixed import in steps

--- features/step_definitions/login_steps.py
+++ features/step_definitions/login_steps.py
@@ -1,3 +1,5 @@
+from lettuce import step, world
+
 @step(u'Given user "([^"]*)" is not logged in')
 def given_user_group1_is_not_logged_in(step, user_name):
     assert False, 'This step must be implemented'
